546de383f567a8f480eb94bbdbdb3eafc4eb3fbf
ptrcmnlgcs <gecosopatricia32@.gmail.com>
2024-06-12 03:53:15 +0800

    Admin Seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -14,20 +14,11 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        // First Admin account
-        User::factory()->create([
-            'name' => 'Admin 1',
-            'email' => 'admin1@gmail.com',
-            'userType' => 'admin',
-            'password' => bcrypt('admin1'), 
-        ]);
-
-        // Second Admin account
-        User::factory()->create([
-            'name' => 'Admin 2',
-            'email' => 'admin2@gmail.com',
-            'userType' => 'admin',
-            'password' => bcrypt('admin2'), 
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'userType' => 'admin', 
         ]);
     }
 }
